337d4cc3a5b2d47363a6774a94a390337e592500
Hov Petrosyan <hovhannes.petrosyan95@gmail.com>
2018-07-04 18:08:42 +0400

    working

---
+++ public/todo.js
@@ -0,0 +1,122 @@
+function render(App,data=''){
+	const props = {
+		header: 'TODO APP',
+		add: 'ADD',
+		del:'DELETE',
+		edit:'SAVE',
+		data:data
+	};
+	const app = new App(props);
+	let root = document.getElementById('root');
+	root.innerHTML += app.render();
+
+}
+
+class Component {
+	constructor(props){
+		this.props = props;
+		window.sendReq = this.sendReq;
+	}
+	sendReq(method,id=''){
+	let data = document.getElementById('inp').value;
+	if(method == 'put')
+	{
+		 data = document.getElementById(id).value;
+		 console.log(data);
+	}
+	let url = '/api/todo/' + id;
+	console.log(url);
+	fetch(url ,{
+		method:method,
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body:JSON.stringify({data:data})
+		}).then(function(res){
+		 			console.log(res);
+		 			res.json().then(function(data){
+		 				console.log(data.data);
+		 					let arr = data.data;
+		 					if(document.getElementById('main_table')){
+		 						document.getElementById('root').removeChild(document.getElementById('main_table'));
+		 					}
+		 					render(Table,arr);
+		 					
+		 		})
+		 		})
+		.catch(function(res){
+			 console.log(res) 
+			});
+	}
+}
+
+class Header extends Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		const {header} = this.props;
+		return '<h1>' + header + '</h1>'; 
+	}
+}
+
+class TODO extends Component{
+	constructor(props){
+		super(props)
+		
+	}
+
+	render(){
+		const {add} = this.props;
+		return '<input name = "todo" id = "inp"><button class="add" onclick="sendReq('+"'post'"+')">'+add+'</button>';
+	}
+}
+
+class Table extends Component{
+	constructor(props){
+		super(props)
+		this.mapItemToRow = this.mapItemToRow.bind(this);
+	}
+
+
+	mapItemsToRows() {
+		const {mapItemToRow} = this;
+		const {data} = this.props;
+		return data.map(mapItemToRow);
+	}
+
+	mapItemToRow(item) {
+		const {del, edit} = this.props;
+		return '<tr><td><input name = "todo" id = '+ item.id +' value = '+item.todo+'></td><td><button class="add" onclick="sendReq('+"'put',"+item.id+')">'+edit+'</button></td><td><button class="add" onclick="sendReq('+"'delete',"+item.id+')">'+del+'</button></td><tr>'
+	}
+
+	render(){
+		const {data,edit,del} = this.props;
+		let display = '<table id = "main_table">';
+		display += this.mapItemsToRows().join("");
+		display+='</table>';
+		return  display;
+	}
+}
+render(Header);
+render(TODO);
+fetch('/api/todos',{
+		method:'get',
+		headers: {
+			"Content-Type": "application/json"
+		},
+		}).then(function(res){
+		 			console.log(res);
+		 			res.json().then(function(data){
+		 				console.log(data.data);
+		 					let arr = data.data;
+		 					if(document.getElementById('main_table')){
+		 						document.getElementById('root').removeChild(document.getElementById('main_table'));
+		 					}
+		 					render(Table,arr);
+		 					
+		 		})
+		 		})
+		.catch(function(res){
+			 console.log(res) 
+			});
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+let todo_arr = [];
+let id = 0;
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+	let f = fs.readFile('/public/index.html',(err,data) => {
+		if (err) throw err;
+		console.log(data);
+		res.send(data);
+	});
+});
+
+app.post('/api/todo/',function(req,res){
+	console.log(req.body);
+	todo_arr.push({todo:req.body.data,id:id++});
+	console.log(todo_arr);
+	res.json({data:todo_arr});
+
+});
+app.delete('/api/todo/:id',function(req,res){
+	console.log('params id',req.params.id);
+		let el = todo_arr.find(function(todo_arr){
+			return todo_arr.id == req.params.id;
+	});
+	console.log('a',el);
+	 todo_arr.splice(todo_arr.indexOf(el),1);
+	res.json({data:todo_arr});
+	console.log('áaa',todo_arr);
+	console.log('dcdc'+req.params.id);
+});
+app.put('/api/todo/:id',function(req,res){
+	
+
+	let el = todo_arr.find(function(todo_arr){
+		return todo_arr.id == req.params.id;
+	});
+	console.log('dcdc'+req.params.id);
+	el.todo = req.body.data;
+	res.json({data:todo_arr});
+
+});
+app.get('/api/todos',function(req,res){
+	res.json({data:todo_arr});
+});
+app.listen(3000);
